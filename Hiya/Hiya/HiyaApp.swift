3c6fade4c3579131e1a23c99d2b2c5c85103430d
craftzcatdev <craftzcatdev@gmail.com>
2026-03-12 21:14:30 +0700

    initial commit: start my iOS journey

---
+++ Hiya/Hiya/HiyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiyaApp.swift
+//  Hiya
+//
+//  Created by Hai Ng. on 12/3/26.
+//
+
+import SwiftUI
+
+@main
+struct HiyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
